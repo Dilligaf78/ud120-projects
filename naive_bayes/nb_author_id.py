13237ccdd16d0440ed38daa5461a1a6c9f4b889a
Dilligaf78 <thecheekybackpacker@gmail.com>
2022-11-30 09:46:15 -0500

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,6 +16,8 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
+
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -24,9 +26,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
 
+# Make a model classifier
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
-
+clf.predict(features_test, labels_test
 ##############################################################
 
 ##############################################################
